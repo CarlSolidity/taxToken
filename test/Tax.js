8ee1dc884e4d82f4584a6032964df28ff4d79c9f
CarlSolidity <carl.charpin@protonmail.com>
2021-09-03 21:54:50 -0400

    Created the contract with a 3% tax to an address

---
+++ test/Tax.js
@@ -0,0 +1,37 @@
+const { expect } = require ('chai');
+//const { ethers } = require('ethers');
+//const { isCallTrace } = require('hardhat/internal/hardhat-network/stack-traces/message-trace');
+
+describe('Tax', function() {
+    // Deploy the contract
+    let owner;
+    let addr1;
+    let addr2;
+    let addrs;
+    
+    beforeEach(async function() {
+        Token = await ethers.getContractFactory("Tax");
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+        tax = await Token.deploy(2, 3);
+        await tax.deployed();
+    });
+
+    describe("Deployment", function() {
+        it("Deploy and check fee parameters", async function () {
+            expect(await tax.feeDecimals.to.equal(2));
+        });
+
+        it("Should assign the total supply of tokens to the owner", async function () {
+            const ownerBalance = await tax.balanceOf(owner.address);
+            expect(await tax.totalSupply()).to.equal(ownerBalance);
+          });
+    });
+
+    describe("Send tokens and check tax collection", function() {
+        it("Send 100 tokens to addr1 after making addr2 tax collector", async function () {
+            await tax.updateTaxCollector(addr2.address);
+            await tax.transferFrom(owner.address, addr1.address, 100);
+        })
+    })
+});
